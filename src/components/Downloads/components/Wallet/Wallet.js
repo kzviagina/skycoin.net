787807a9b23a1eac73e156c524198af6b1aead07
Alexey Belous <alexey.belous@sigma.software>
2018-03-14 18:07:30 +0200

    Fix wallets page headings.

--- src/components/Downloads/components/Wallet/Wallet.js
+++ src/components/Downloads/components/Wallet/Wallet.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Box } from 'grid-styled';
 import { FormattedMessage } from 'react-intl';
+import { rem } from 'polished';
 
 import Container from 'components/Container';
 import Heading from 'components/Heading';
 import SubHeading from 'components/SubHeading';
 import ApplicationTable from 'components/ApplicationTable';
 
+import { FONT_SIZES } from 'config';
 import { downloads, version } from './downloads';
 
 const Wallet = () => (
   <div>
     <Container>
       <Box width={[1 / 1, 1 / 1, 2 / 3]} my={[5, 7]}>
-        <Heading heavy as="h2" fontSize={[5, 6]} color="black" mb={[1, 2]}>
+        <Heading heavy as="h2" fontSize={[rem(FONT_SIZES[10]), rem(FONT_SIZES[10])]} color="black" mb={[1, 2]}>
           <FormattedMessage id="downloads.wallet.heading" />
         </Heading>
 
